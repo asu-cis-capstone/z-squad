ad6f4acbead66a422bb884af44b50921ba8bb1b0
bamercad <bamercad@asu.edu>
2015-03-12 22:12:42 -0700

    process.php variable fix

--- Zoophy/process.php
+++ Zoophy/process.php
@@ -30,7 +30,7 @@
 	session_start('user');
 
 	$row = mysqli_fetch_array($result);
-	$_SESSION['user'] = $row['name'];
+	$_SESSION['user'] = $row['fName'.'lName'];
 	header('Location: main.html');
 	exit;
 
