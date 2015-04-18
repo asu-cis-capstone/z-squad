6e751661132f10e60e8c1944d5497a59fe69f705
Hunter Bryan <hgbryan@asu.edu>
2015-04-18 15:59:01 -0700

    Solved search.aspx Parse errors
    CodeBehind to CodeFile, Inherits "WebForm3" to "search" in search.aspx
    (top of file), public partial class "search" instead of "WebForm3" in
    search.aspx.cs

--- Demo 1.2/Demonstration/search.aspx.cs
+++ Demo 1.2/Demonstration/search.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Demonstration
 {
-    public partial class WebForm3 : System.Web.UI.Page
+    public partial class search : System.Web.UI.Page // changed from WebForm3 to search, that solved the Parse error
     {
         string virus;
         string gene;
