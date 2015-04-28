5afb9f77aa935e4f68d3160b04385dc00915ef86
bamercad <bamercad@asu.edu>
2015-04-27 22:10:15 -0700

    treeDownload

---
+++ Demo 1.2/Demonstration/treeDownload.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Demonstration
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string tree = "carnivore.tree";
+            string globString = Request.MapPath("../tree"). 
+            
+            Response.AppendHeader("content-disposition", "attachment; filename=" + fileName);
+        }
+    }
+}
